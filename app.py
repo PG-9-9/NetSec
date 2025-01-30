182cb930a38e1301d6eaef6cf7f1f437bb6d637a
PG-9-9 <vishaal.saravanan@fau.de>
2025-01-30 11:26:10 +0100

    Added batch prediction using Jenga

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import sys, os
 from dotenv import load_dotenv
 import certifi
 import pymongo
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
 from networksecurity.utils.main_utils.utils import load_object
 from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile,Request
+from fastapi.templating import Jinja2Templates
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
@@ -29,6 +33,7 @@ client=pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 database=client[DATA_INGESTION_DATABASE_NAME]
 collection=database[DATA_INGESTION_COLLECTION_NAME]
 
+
 # Create a FastAPI instance
 app=FastAPI()
 origins = ["*"]
@@ -39,6 +44,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Create a Jinja2 template object
+templates=Jinja2Templates(directory="./templates")
 
 # Generic API home page
 @app.get("/", tags=["authentication"])
@@ -54,6 +62,30 @@ async def train_route():
         return Response('Training Pipeline Completed Successfully')
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+
+# API to predict the model   
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
     
 if __name__ == "__main__":
     # Run the FastAPI application
